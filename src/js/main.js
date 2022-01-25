e5708f9559051b6a6486e85e1dba969e1f70dd77
ntsmwk <markus.weissenbek@gmx.at>
2022-01-25 12:59:27 +0100

    ..

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import '/_site/css/style.css';
 
 import Alpine from 'alpinejs';
 import intersect from '@alpinejs/intersect';
+import 'tw-elements';
 
 Alpine.plugin(intersect);
 Alpine.start();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  content: ["_site/**/*.html", "_site/**/*.js"],
+  content: [
+    '_site/**/*.html',
+    './node_modules/tw-elements/dist/js/**/*.js',
+    '_site/**/*.js',
+  ],
+  plugins: [require('tw-elements/dist/plugin')],
 };
