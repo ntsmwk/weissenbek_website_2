63a126b880cb5a30280db409bc8436c97812bef0
Markus Weissenbek <markus.weissenbek@gmx.at>
2022-01-31 15:04:42 +0100

    added jquery

--- src/js/main.js
+++ src/js/main.js
@@ -3,19 +3,12 @@ import '/_site/css/style.css';
 import Alpine from 'alpinejs';
 import intersect from '@alpinejs/intersect';
 import 'tw-elements';
+import $ from "jquery";
 
 Alpine.plugin(intersect);
 Alpine.start();
 
-document.querySelectorAll('[data-toggle="lightbox"]').forEach(function (e){
-  e.addEventListener('click', function(event) {
-    event.preventDefault();
-    this.ekkoLightbox();
-
-  })
-})
-
-// $(document).on('click', '[data-toggle="lightbox"]', function (event) {
-//   event.preventDefault();
-//   $(this).ekkoLightbox();
-// });
+$(document).on('click', '[data-toggle="lightbox"]', function (event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
