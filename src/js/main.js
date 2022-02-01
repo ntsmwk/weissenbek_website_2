e36cb861246398f646a51a88e633768a2a184d4f
ntsmwk <markus.weissenbek@gmx.at>
2022-02-01 18:10:28 +0100

    ..

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,6 @@ import '/_site/css/style.css';
 import Alpine from 'alpinejs';
 import intersect from '@alpinejs/intersect';
 import 'tw-elements';
-import $ from "jquery";
 
 Alpine.plugin(intersect);
 Alpine.start();
-
-$(document).on('click', '[data-toggle="lightbox"]', function (event) {
-  event.preventDefault();
-  $(this).ekkoLightbox();
-});
